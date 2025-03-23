500db706a9ca6a65d53ddd4ad501ff79e991c487
XxpolipxX <adam.gabrys@poczta.onet.pl>
2025-03-23 12:38:14 +0100

    Initial commit

---
+++ fremcss/scripts/main.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".code").forEach(codeBlock => {
+        let button = document.createElement("button");
+        button.classList.add("copy_button");
+        button.innerText = "Kopiuj kod";
+
+        button.addEventListener("click", () => {
+            let codeElements = codeBlock.querySelectorAll("pre, p");
+            if(!codeElements.length) return;
+
+            let codeText = Array.from(codeElements)
+                .map(el => el.innerText)
+                .join("\n");
+
+            navigator.clipboard.writeText(codeText).then(() => {
+                button.innerText = "Skopiowano";
+                setTimeout(() => button.innerText = "Kopiuj kod", 2000);
+            });
+        });
+
+        codeBlock.prepend(button);
+    });
+});
